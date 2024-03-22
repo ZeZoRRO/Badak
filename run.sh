01935cc5de9f1ae2e8e884e980a2c523d7b6199c
ZeZoRRO <162972606+ZeZoRRO@users.noreply.github.com>
2024-03-22 13:50:24 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-e70a4afa-2808-4f3e-9d1c-4bb9f0f2e0e4/)
 
 
 echo "$curl_output" >> Update.tsx
